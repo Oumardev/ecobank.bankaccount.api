7e30222eb7da4a7f0963004cf59173f4e79d2982
Oumardev <oumsdev@gmail.com>
2022-05-21 22:58:40 +0000

    new text y

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('Hey welcome new version is deploy')
+    res.send('Hey welcome new version is deploy ok last test')
 })
 
 app.listen(4200,'86.107.197.161',()=>{

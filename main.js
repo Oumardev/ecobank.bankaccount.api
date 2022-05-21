348927c8d9e93cf70226ea9a3bd8e510bc9f42d4
Oumardev <oumsdev@gmail.com>
2022-05-21 22:53:25 +0000

    main ip

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ app.get('/',(req,res)=>{
     res.send('Welcome to Apoloan release API new version')
 })
 
-app.listen(4200,'localhost',()=>{
+app.listen(4200,'86.107.197.161',()=>{
     console.log('server is running on http://localhost:4200')
 })
